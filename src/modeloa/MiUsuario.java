fd712112a636143b05f4e755c92df3b25a661e0a
Lander <Lander@LanderFI>
2020-04-28 11:41:17 +0200

    MiUsuario java klasea eginda

---
+++ src/modeloa/MiUsuario.java
@@ -0,0 +1,15 @@
+package modeloa;
+
+import modelo.bean.Usuario;
+
+public class MiUsuario extends Usuario {
+	
+	public boolean validar() {
+		if(super.getCodigo().length() == 4 && super.getDni().length() == 9) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+}
